f572edd753d3e8ca6e5dada54a896b258ef4587d
Peae Chan <peaechan@gmail.com>
2026-01-19 11:53:32 +0800

    Update info in SettingsForm

--- ScreenSaver/SettingsForm.Designer.cs
+++ ScreenSaver/SettingsForm.Designer.cs
@@ -26,14 +26,15 @@ namespace ScreenSaver
             this.okButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.dynamicLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // textBox
             // 
-            this.textBox.Location = new System.Drawing.Point(26, 162);
-            this.textBox.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox.Location = new System.Drawing.Point(20, 156);
             this.textBox.Name = "textBox";
-            this.textBox.Size = new System.Drawing.Size(441, 22);
+            this.textBox.Size = new System.Drawing.Size(332, 22);
             this.textBox.TabIndex = 0;
             // 
             // label1
@@ -41,40 +42,36 @@ namespace ScreenSaver
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(20, 16);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(15, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 31);
+            this.label1.Size = new System.Drawing.Size(239, 25);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Web Screen Saver";
+            this.label1.Text = "Web Screen Saver v2.0";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 58);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(21, 44);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(194, 17);
+            this.label2.Size = new System.Drawing.Size(136, 12);
             this.label2.TabIndex = 2;
             this.label2.Text = "Rotate URLs as screen saver";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 141);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(21, 140);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 17);
+            this.label3.Size = new System.Drawing.Size(60, 12);
             this.label3.TabIndex = 3;
             this.label3.Text = "Source File:";
             // 
             // okButton
             // 
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(259, 192);
-            this.okButton.Margin = new System.Windows.Forms.Padding(4);
+            this.okButton.Location = new System.Drawing.Point(194, 178);
             this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(100, 28);
+            this.okButton.Size = new System.Drawing.Size(75, 21);
             this.okButton.TabIndex = 4;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
@@ -84,10 +81,9 @@ namespace ScreenSaver
             // 
             this.cancelButton.CausesValidation = false;
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(367, 192);
-            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Location = new System.Drawing.Point(275, 178);
             this.cancelButton.Name = "cancelButton";
-            this.cancelButton.Size = new System.Drawing.Size(100, 28);
+            this.cancelButton.Size = new System.Drawing.Size(75, 21);
             this.cancelButton.TabIndex = 5;
             this.cancelButton.Text = "Cancel";
             this.cancelButton.UseVisualStyleBackColor = true;
@@ -97,20 +93,43 @@ namespace ScreenSaver
             // 
             this.dynamicLinkLabel.ForeColor = System.Drawing.Color.Blue;
             this.dynamicLinkLabel.LinkArea = new System.Windows.Forms.LinkArea(0, 60);
-            this.dynamicLinkLabel.Location = new System.Drawing.Point(28, 75);
+            this.dynamicLinkLabel.Location = new System.Drawing.Point(21, 56);
+            this.dynamicLinkLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.dynamicLinkLabel.Name = "dynamicLinkLabel";
-            this.dynamicLinkLabel.Size = new System.Drawing.Size(331, 23);
+            this.dynamicLinkLabel.Size = new System.Drawing.Size(248, 17);
             this.dynamicLinkLabel.TabIndex = 6;
             this.dynamicLinkLabel.TabStop = true;
             this.dynamicLinkLabel.Text = "https://github.com/pgnunes/web-screen-saver/";
             this.dynamicLinkLabel.UseCompatibleTextRendering = true;
             this.dynamicLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkedLabelClicked);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Forked by peaechan";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(21, 100);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(218, 12);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/peaechan/web-screen-saver";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(480, 239);
+            this.ClientSize = new System.Drawing.Size(360, 211);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.label3);
@@ -119,7 +138,6 @@ namespace ScreenSaver
             this.Controls.Add(this.textBox);
             this.Controls.Add(this.dynamicLinkLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "SettingsForm";
             this.Text = "ScreenSaver Settings";
@@ -142,6 +160,15 @@ namespace ScreenSaver
         {
             dynamicLinkLabel.LinkVisited = true;
             System.Diagnostics.Process.Start("https://github.com/pgnunes/web-screen-saver/");
+        }
+
+        private Label label4;
+        private LinkLabel linkLabel1;
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            dynamicLinkLabel.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/peaechan/web-screen-saver/");
         }
     }
 }
